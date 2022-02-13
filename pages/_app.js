39b9a6ea8dd143b7e53d97e2556c3e2e3fafceb1
Reekraj Roy <55712612+REEKRAJROY@users.noreply.github.com>
2022-02-13 11:41:33 +0530

    Add files via upload

---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import App from 'next/app';
+import '../styles/globals.scss';
+import { Layout } from '../components';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default MyApp;
